a27b155f0c3643ef3edceed00ec742ef7bb415af
brunoLloret <163921771+brunoLloret@users.noreply.github.com>
2024-06-19 18:24:57 -0400

    mechanics+calculators
    Mechanics almost done (except Mazo) --needs to be revised in order to check the logic of players
    Calculators done

---
+++ Truco/Truco-01/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="App">
+        <button className="btn">Hello daisyUI</button>
+      </div>
+    </>
+  )
+}
+
+export default App
